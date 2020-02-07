2a4626dfbd161a4998cabda52bfa705105fe473c
antonio.martin <antonio.martin@schibsted.com>
2020-02-07 19:32:56 +0100

    register globally components

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -4,6 +4,11 @@ import Vuikit from '../dist-uikit/uikit.min.js'
 import VuikitIcons from '../dist-uikit/uikit-icons.min.js'
 import VuikitCss from '../dist-uikit/uikit.min.css'
 
+// register globally
+Vue.component('Vuikit', Vuikit)
+Vue.component('VuikitIcons', VuikitIcons)
+Vue.component('VuikitCss', VuikitCss)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
