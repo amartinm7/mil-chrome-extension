b30141d91d7322a1b0b3ffefaf4602c9b36f0166
antonio.martin <antonio.martin@schibsted.com>
2020-02-12 00:36:20 +0100

    refactoring tests

---
+++ __tests__/popup/application/GetMyFavouriteAdsServiceShould.js
@@ -0,0 +1,16 @@
+import getMyFavouriteAdsService from "../../../src/popup/application/GetMyFavouriteAdsService"
+import findAllMyFavouritesAds from "../../../src/popup/framework/GetMyFavouritesAdsRepository"
+import global from "../global.json"
+const assert = require("assert")
+
+console.log("welcome! GetMyFavouriteAdsService test")
+
+describe('GetMyFavouriteAdsService', function() {
+    describe('getMyFavouritesAdsRepository', function() {
+        it('should return a valid ad', async function() {
+            const ads = await getMyFavouriteAdsService(findAllMyFavouritesAds, global.apiToken)
+            console.log(JSON.stringify(ads.data))
+            expect(ads).not.toBeUndefined()
+        })
+    })
+})
--- __tests__/popup/global.js
+++
@@ -1,4 +0,0 @@
-export default global = {
-    apiToken : "<gsRPOICL_YOVUY8fntlAQzD1yVHiWLw0mL102]T@\\Y3WiL?IOk0f0luDzAF5l8ip2WP\\18LYxnwCrjR\\=WU`qO=j`ybS_V6mp399f^mVwy4ap75rOmu1<ONwP6ERND<"
-
-}
