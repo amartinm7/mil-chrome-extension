94c9f7edd0ed74907ea95e23fbc92db161c01393
antonio.martin <antonio.martin@schibsted.com>
2020-02-19 23:32:53 +0100

    adding user email property to see in the presentation layer

--- src/popup/application/user/DoLoginWithCookiesService.js
+++ src/popup/application/user/DoLoginWithCookiesService.js
@@ -18,7 +18,10 @@ class DoLoginWithCookiesService {
         console.log(">>>_executeDoLoginWithCookies")
         const doLoginWitCookiesResponse = await this._doLoginWithCookiesRepository.doLoginWithCurrentCookies()
         console.log(`>>> doLoginWitCookiesResponse ${JSON.stringify(doLoginWitCookiesResponse)}`)
-        return new DoLoginWithCookiesServiceResponse({apiToken: doLoginWitCookiesResponse.data.apiToken})
+        return new DoLoginWithCookiesServiceResponse({
+            apiToken: doLoginWitCookiesResponse.data.apiToken,
+            email: doLoginWitCookiesResponse.data.user.email
+        })
     }
 }
 
