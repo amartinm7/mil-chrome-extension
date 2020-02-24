a268971809712ebb54ee200bba4b1bfe7d476922
antonio.martin <antonio.martin@schibsted.com>
2020-02-24 22:07:00 +0100

    fixing test

--- __tests__/popup/framework/repository/SaveStorageRepositoryShould.js
+++ __tests__/popup/framework/repository/SaveStorageRepositoryShould.js
@@ -1,10 +1,9 @@
 const assert = require("assert")
 import './using-localstorage.test.js'
-import BrowserRepository from "../../../../src/popup/framework/repository/storage/SaveStorageRepository"
 
 console.log("welcome! SaveStorageRepository test")
 
-describe('BrowserRepository', function() {
+describe('SaveStorageRepository', function() {
     describe('execute', function() {
         afterEach(() => {
             localStorage.clear();
