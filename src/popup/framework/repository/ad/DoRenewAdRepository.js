578df4771a6873bacba22c631d343928a6b119b2
antonio.martin <antonio.martin@schibsted.com>
2020-02-27 22:04:55 +0100

    implementing doRenew use case

--- src/popup/framework/repository/ad/DoRenewAdRepository.js
+++ src/popup/framework/repository/ad/DoRenewAdRepository.js
@@ -2,17 +2,12 @@ import axios from "axios"
 import qs from 'querystring'
 
 class DoRenewAdRepository {
-    async _getHeaderAndQueryParams (apiToken,sessionId){
+    async _getHeaderAndQueryParams (apiToken){
         console.log(">>>DoRenewAdRepository.getHeaderAndQueryParams")
         const params =  {
-            "tokenRenew" : ' '
+            "tokenRenew": " "
         }
         const header = {
-            "mav": "2",
-            "Accept": "*/*",
-            "Cache-Control": "no-cache",
-            'Content-Type': 'application/json',
-            'token': `Basic ${apiToken}`,
             'Authorization': apiToken
         }
         console.log(">>>DoRenewAdRepository.getHeaderAndQueryParams:" + JSON.stringify({header, params}))
@@ -24,15 +19,13 @@ class DoRenewAdRepository {
     async doRenewAd (doRenewAdRepositoryRequest) {
         console.log(">>>DoRenewAdRepository.doRenewAd")
         const vm = this
-        const { header, params } = await this._getHeaderAndQueryParams(doRenewAdRepositoryRequest.apiToken, doRenewAdRepositoryRequest.sessionId)
-        console.log("h&qp:" + JSON.stringify({ header, params }))
-        const url = `https://www.milanuncios.com/v3/ads/${doRenewAdRepositoryRequest.adId}/renew`
+        const { header, params } = await this._getHeaderAndQueryParams(doRenewAdRepositoryRequest.apiToken)
+        const url = `https://www.milanuncios.com/api/v3/ads/${doRenewAdRepositoryRequest.adId}/renew`
         const axiosRequest = {
             method: 'post',
             url: url,
             data: params,
-            headers: header,
-            config: { withCredentials: true }
+            headers: header
         }
         console.log(`axiosRequest: ${JSON.stringify(axiosRequest)}`)
         return axios(axiosRequest)
