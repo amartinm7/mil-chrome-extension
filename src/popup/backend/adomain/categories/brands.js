9b9dc33dddb35312b298828053248e6f4c1cee36
antonio.martin <antonio.martin@schibsted.com>
2020-05-11 18:17:11 +0200

    018 adding categories

---
+++ src/popup/backend/adomain/categories/brands.js
@@ -0,0 +1,738 @@
+Volkswagen,43085
+BMW,42309
+Mercedes-Benz,41476
+Audi,35067
+Renault,34432
+Peugeot,32553
+Citroen,32538
+Ford,31308
+Seat,30753
+Opel,30585
+Nissan,15853
+Toyota,13015
+Fiat,12213
+Hyundai,10915
+Kia,9603
+"",9096
+Mini,7296
+Volvo,6911
+Land-Rover,5458
+Skoda,5377
+Mazda,4667
+Porsche,4380
+Honda,3922
+Mitsubishi,3353
+Alfa Romeo,3306
+Jaguar,3146
+Chevrolet,3066
+Jeep,3024
+Suzuki,2743
+Dacia,2410
+Smart,2241
+Lexus,2157
+Chrysler,2075
+Ssangyong,2024
+DS,987
+SAAB,971
+Subaru,956
+Daewoo,694
+Lancia,669
+INFINITI,646
+Rover,642
+Maserati,496
+Dodge,427
+Ferrari,363
+ABARTH,337
+Iveco,312
+Bentley,243
+MG,223
+Cadillac,220
+Tata,191
+GALLOPER,148
+ASTON MARTIN,133
+Lamborghini,130
+TESLA,102
+Hummer,99
+Isuzu,85
+Mahindra,80
+Pontiac,64
+Rolls-Royce,61
+Lada,60
+CUPRA,57
+Austin,53
+Lotus,51
+Daihatsu,45
+Mercedes,39
+otra marca,35
+Talbot,33
+Otra,33
+CORVETTE,32
+santana,31
+Alfa ,21
+Triumph,19
+Lincoln,19
+Iveco-pegaso,19
+PIAGGIO,17
+ASIA,16
+DFSK,16
+ARO,16
+MCLAREN,15
+Bertone,13
+Ficha tecnica,13
+Morgan,12
+todos,11
+Aixan,10
+Buick,10
+Todas marcas,9
+DAF,9
+Hurtan,9
+Simca,8
+UMM,8
+C0mpram0s c0ches,7
+MAYBACH,7
+Autobianchi,7
+Yamaha,7
+Transporte,7
+Wartburg,6
+Asia motors,6
+Daimler,6
+GMC,6
+JDM,6
+Varios,5
+Alpine,5
+Ficha tecnica reduci,5
+Oldsmobile,5
+comarth,4
+ESTRIMA BIRO,4
+Compro,4
+",,,,,máxima tasazion",4
+ford transit,4
+nissan navara,4
+Renaul,4
+Maxima tasaciÓn,4
+alquiler de coches,4
+mercedes benz,4
+Iveco daily,4
+Compramos coches,4
+Todas las marcas,3
+Transportes,3
+Plymouth,3
+Scania,3
+Liquidacion,3
+Carbodies,3
+Portaperros,3
+Alquiler,3
+ficha reducida,3
+Compramos tu coche,3
+Range rover,3
+grua,3
+Todas,3
+Elevador,3
+Los precios mas bajo,3
+Motor,3
+Autos,3
+Citroen Berlingo,3
+Opel corsa,3
+Pegaso,3
+Tapiceria,3
+BAJA,3
+Micro car,3
+Zimmer,3
+Desguace,3
+BEISUTE,3
+Transferencias,3
+malcor,3
+Multimarca,2
+Kyosho,2
+Renault trafic,2
+Tcm,2
+Cobra,2
+Coche clasico,2
+gruas,2
+DS Automobiles,2
+Fiat - scudo expert,2
+Grandes ocasiones,2
+Importacion,2
+te interesa saber,2
+Automoviles,2
+Coches,2
+Varias,2
+Rematricular,2
+EBRO,2
+Plataforma,2
+Busco coche,2
+.,2
+vendo web y dominio,2
+Vende tu coche,2
+ACURA,2
+VAZ,2
+Microcar MC2,2
+Kart cross,2
+Matricular vehiculo,2
+Tazzari,2
+Grúa alicante,2
+Toyota avensis 117.000 kms!!!,2
+Renault kangoo,2
+Matricular,2
+RENAULT V.I.,2
+Green tour,2
+Gme,2
+Gestoria,2
+Auverland,2
+AVIA,2
+Barreiros,2
+Iveco dayli,2
+SAVA,2
+Bellier,2
+Peugeot partner,2
+Coche bodas,2
+Vw,2
+LOTE,2
+Portes,2
+Honda crv 4x4,2
+busco inversor,2
+Portaequipaje,2
+Ford Focus,2
+Vauxhall,2
+LIGIER,2
+Citroen berlingo HDI,2
+Compro coches,2
+Miniauto,2
+LTI,2
+Kawasaki,2
+Ficha tecnicas,2
+morris,2
+Benimar,2
+FORD A  1929,2
+Todo tipo de coches,2
+ITV,2
+westfield,2
+Bugatti,2
+Trigger,2
+El precio mas bajo,2
+Packard,2
+Peugeot 406,1
+Triumph MK II,1
+Fiat Stilo,1
+Morgan 4/4,1
+Xara,1
+Audi A3 Cabriolet,1
+Daimler xj,1
+tdi,1
+For,1
+Wolf,1
+Michelin,1
+Derbi gpr de 50,1
+se busca,1
+transportes de vehic,1
+Asientos,1
+Mercedes vito 112,1
+Golf 4,1
+Motor 1,1
+Xsara,1
+Coches de,1
+Microcar y ligier,1
+PSA,1
+Otro,1
+Internacional,1
+volkswagen-seat,1
+Chapa y pintura,1
+Audi seat Peugeot,1
+Vehiculo,1
+Lote vehiculos,1
+Ursus,1
+Apero,1
+iveeco,1
+Trhiump,1
+Seat 600,1
+hurmigonera,1
+Winway,1
+Taxi,1
+las vegas,1
+Afikim,1
+BMW SERIE 530 D 1.800 E. OCASIÓN!!!,1
+servicios grua,1
+Eton,1
+Mercedes benz clase a!!!,1
+Volkswagen polo,1
+Opel insignia,1
+Pictor,1
+Saab 93 mantenimientos al dÍa!!!,1
+Ofertones,1
+Wolsvgen,1
+Eveco,1
+Caja de cambio...,1
+Opel vivaro bi turbo,1
+peritaciones,1
+Moto zodiad,1
+top kart,1
+"Melex,Club Car,Ezgo",1
+Seguros,1
+0pel,1
+InstalaciÓn,1
+Por solo 4.900€,1
+Lancia phedra,1
+obd2,1
+vehiculo importado,1
+buggy 250,1
+Seguro de coche,1
+Scrambler,1
+Tintado de lunas,1
+lamina solar,1
+Descapotable para bo,1
+Ddfsk,1
+Japtec,1
+Sterling,1
+Lincoln LS,1
+Audi A4,1
+Audi opel renault,1
+Citroen c3,1
+Beberly,1
+Indiferente,1
+CorredurÍa,1
+Mini cooper 30 aniversario,1
+Replica de Bugatti,1
+Renault scenic 7 plazas 1.900 euros,1
+Prm fum bost gt,1
+Suzuki.,1
+Chattenet,1
+Coche antiguo,1
+Licencia VTC,1
+Coche,1
+Servicio grua,1
+huma,1
+PRO,1
+Enganches,1
+Bertolini,1
+clenet  2,1
+Robin T-350,1
+VehÍculos,1
+Ruta - Excursión /,1
+( macroautos ),1
+Para tipo de vehÍcul,1
+"Citroen, ford,renaul",1
+Chevy,1
+Hergo,1
+Deguace65,1
+Tombalife,1
+"Seat, renault, opel",1
+Mini clasico,1
+Volkswagen golf 2.3 v5,1
+Coche de gasolina,1
+Peugeot citroen,1
+Venta,1
+RAM,1
+BMW 320 I 2.200 E. OCASIÓN!!!,1
+Citron,1
+Liger xtoor,1
+Mercedes s 300 td 177cv aut-pie-nav,1
+CHATENET,1
+OPEL CORSA 1.400 E. CON ITV!!!!,1
+Seat Ibiza,1
+Vauxhall opel,1
+Volkswagen golf 4 1.200 euros!!!,1
+Buggy PGO,1
+DS 4,1
+Mazda 6 edition full,1
+Bmw vw audi mercedes,1
+Axiam,1
+Ds 3,1
+Cuadriciclo  erad,1
+Jiayuan,1
+Piquersa,1
+Fiat adaptada,1
+Caterham,1
+SEAT  600 E,1
+Volkswagen audi seat,1
+Todass,1
+"Opel, nissan, volvo",1
+Grua porta coches,1
+Furgonetas,1
+Sanyo,1
+SHELBY,1
+Alquiler cabina,1
+Lancia delta,1
+Ltd austin farwey,1
+CitroËn c5 itv en vigor!! 1.700 eur,1
+Carruaje,1
+Taxi london,1
+Coche carreras,1
+Caliber,1
+hardi,1
+Quicksilver,1
+Renault Clio,1
+Peugeot 807 adaptada,1
+Piagio,1
+Escapes a medida,1
+Gran,1
+NeumÁticos,1
+Opel zafira,1
+Maxus,1
+PSA-Peugeot-Citroën,1
+KTM,1
+Vendo este bonito,1
+Sin rodeos,1
+Escarabajo golf,1
+Gem 4 luxe,1
+BsCat,1
+Sinca,1
+Taller,1
+Coche pequeÑo,1
+Outlet familiares y,1
+Alquiler coche desde,1
+Sat Enterprisse,1
+Bombardier,1
+Celtaquatre,1
+Mercedes S 320,1
+averiados,1
+AVAT,1
+Volkswagen golf,1
+flex,1
+Wrangler,1
+Mecanica,1
+mercedes 350s,1
+Varias marcas,1
+Peugeot 206 poco consumo!!!!,1
+Higland,1
+chrysler  voyager,1
+Se alquila ropa,1
+para bodas,1
+seat-renault otras m,1
+Seat toledo,1
+Launch 431,1
+varios coches,1
+Multigremios,1
+Teide,1
+Fotovoltaica,1
+Peugeot bmv,1
+Nisan,1
+Manitas,1
+Iveco dayli 35.8,1
+Ayxam,1
+Modulos-kit,1
+Equipo de asistencia,1
+Vendo compro,1
+LOTE coches remolque,1
+Coches varios,1
+Bentley arnage,1
+Dacia 1.6 75cv,1
+Oper,1
+Portavehículos,1
+mercedes.slk.2.300.200cv.,1
+Yundai,1
+Today sunshine,1
+Bobcat s130,1
+MAN,1
+FASI 140,1
+Abaca,1
+Liger,1
+Old Car,1
+Car cros,1
+"Vw,Fíat,alfa,skoda",1
+JBA,1
+Kaiser-Frazer,1
+Itv girona,1
+Carcross,1
+Chrysler voyaguer,1
+Lingier,1
+Pack 3 coches,1
+",,,,,desguaze-",1
+Monoplaza,1
+Gestión venta,1
+"Toda clase,",1
+Legend car,1
+e auto,1
+Cambio 2,1
+Tdi110,1
+Tractor Bjr,1
+Kartcros,1
+Fordtrasit,1
+super ofertas,1
+Ford mondeo 2.100 euros!!!,1
+Innocenti,1
+Land Rover Ranger rover,1
+Coche 49,1
+Seat león,1
+Vauxhall viva,1
+Cambio coches,1
+Acero,1
+kia carnival 2.9 crd,1
+Coche funebre,1
+Ruedas coche,1
+Seguros y financiar,1
+Fisker,1
+Detector,1
+mercedes vaneo,1
+Renault laguna,1
+Dama de elche,1
+limunsina lincoln,1
+",,,,desguace",1
+Cambio caja bmw,1
+Servicio de grua,1
+MEGA,1
+"Bmw,audi,ford,seat,",1
+Lote coche,1
+Autocaravana fiat,1
+00000,1
+BMV,1
+Hydro ecologic,1
+camper,1
+Range rover.,1
+bmw  530,1
+Microcar aixam,1
+Volswagen,1
+Mercedes sl,1
+ruedas,1
+Nissan Vanette,1
+Itv vehiculo,1
+SAAB 93 150.000 KMS OCASIÓN!!!,1
+Manitu,1
+Golf,1
+FK,1
+Suncar,1
+volkwagen,1
+Coches para bodas,1
+B M W,1
+azel,1
+Buggy,1
+Audi tt,1
+symca,1
+volkswagen golf cabrio,1
+Sin Carnet,1
+Utilitario,1
+evinrude,1
+gruas en alcala,1
+Se compra,1
+Remolque,1
+Bitter,1
+Cactus,1
+VIDI,1
+URO,1
+Limusina,1
+MICROCAR,1
+Venta vtc-,1
+AUDI A3 Cabrio,1
+E-bike,1
+Citroen  c15,1
+Yacar cross,1
+Sear,1
+Delka,1
+Mini Rover,1
+Caterpillar,1
+Renault wind,1
+Joyner,1
+Eniak,1
+Remolques,1
+Mecanico,1
+aa,1
+Grupo mir,1
+DKW,1
+Zastava,1
+Nanfang,1
+PLUMA,1
+Vehiculos desde,1
+Tarjeta vtc,1
+Marbella,1
+AIXAM,1
+Venta vehiculos 2º,1
+Matriculacion de,1
+Deguace,1
+Motos y Patinetes,1
+Datsun,1
+V.O.,1
+Nem motors,1
+Moto,1
+microcar mc1,1
+Proto trial,1
+Renault  14,1
+Bmw 530 d 1.800 e. ocasiÓn pamplona,1
+Marcos,1
+Deutz,1
+Mercedes benz CLASE SLK 135.000 KMS,1
+Fiat grande punto,1
+Opel asstra,1
+Colibrí,1
+Seat inca,1
+Whippet,1
+I.t.v,1
+Homologar,1
+Volkswagen passat automÁtico!!,1
+informe periciales,1
+Car,1
+Mianuto,1
+"Audi,seat,bmw,",1
+Elka,1
+Fichas reducidas,1
+DG oiki,1
+thriump,1
+Hyundai i30 full,1
+citroen c8,1
+",,,,,desguace""",1
+Aixam minauto,1
+Compramos y vendemos,1
+Kia Carnival,1
+Compra de vehículos,1
+Vehiculos importados,1
+Delphi,1
+Ford cougar tuning,1
+Nissan trade,1
+Informática,1
+limusina Lincoln town car,1
+Towbox,1
+Nissan pulsar,1
+Aisan,1
+Super seven,1
+Im30,1
+Mb brabus,1
+Legend Race car,1
+CitroËn c2 automÁtico 140.000 kms!!,1
+Virgo,1
+Carbodies fx4  fairw,1
+Megan,1
+Renault scenic 1.600 euros pamplona,1
+Xingling,1
+Se Alquila Furgoneta,1
+Zastava yugo,1
+C5,1
+Hyonaung,1
+Megane sport,1
+Allard,1
+Vendo pagina web,1
+New holland,1
+Bango,1
+Servicio de grúa eco,1
+Minauto,1
+",,,,desguaze$",1
+Autogas italia glp,1
+Compra,1
+Ficha tecnica  REDUC,1
+Deguace44,1
+peritamos vehiculos,1
+"Lancia, Mercedes",1
+Astra,1
+Camperizaciones,1
+Furgon,1
+Singer,1
+Inocenti,1
+Coches baratos,1
+Lancia Delta 3,1
+Deposito,1
+Renault Clío 1.2,1
+Gmc yukon xl,1
+Fiat stilo transferencia incluida!!,1
+Microcar m2,1
+Mercedes clk320,1
+coche segundamano,1
+Quickie,1
+PGO SCOOTERS,1
+Volkswagen.,1
+Multi,1
+Opel grandland x,1
+Chataner,1
+Lombardini,1
+Opel-Astra,1
+Alquiler de,1
+EnglishSpanish,1
+Opel tigra 1.4 16V 90cv coupe,1
+citroen c4 adaptado,1
+Certificado coc,1
+Compramos,1
+Oficial,1
+Lote coches,1
+Car croos,1
+Kart Tony kart,1
+Trabant,1
+Liguier too max,1
+Asistencia,1
+Scoda,1
+maranello,1
+"Renault , opel, citr",1
+Jardinera murciana,1
+Linconl,1
+"Honda,Volkswagen,sea",1
+Nissan autos 3b,1
+Rolls royce,1
+LDV,1
+Hyper,1
+Land rover desaparec,1
+Adler,1
+Talleres Davila,1
+Elevador cascos,1
+Compair,1
+Oferton,1
+Ocasion!!,1
+bbs,1
+trimack,1
+Kymco,1
+Renault adaptada,1
+",,,,,desguace",1
+Epoxi suelo,1
+",,,,máxima tasazion",1
+Reducida,1
+Kinko,1
+vespa,1
+Busco,1
+Desguacee,1
+ac cobra,1
+Kimko,1
+Habitacion,1
+Kymco/aixam,1
+"fiat punto , varias",1
+A,1
+Hurtu,1
+Chevrolet express 1500,1
+Super oferta,1
+Ford lincoln town car,1
+Monopatin,1
+Can am,1
+Mercedes benz slk 135.000 kms!!!,1
+Conductor,1
+Smart forfour 135.000 kms!!!,1
+Ronda,1
+Volvo.   940 gle y turbo GLE,1
+Ford fiesta 700 euros itv en vigor!,1
+Chrysler town countr,1
+Conduce GRATIS,1
+Rieju,1
+BMW seat citron jagu,1
+Launch,1
+renault ford audi,1
+UAZ,1
+Vendo o cambio,1
+Alambradas ciudad re,1
+Todo,1
+",,,,,máxima tasacion",1
+La gran tienda,1
+Coche furgonetas,1
+Rover mini,1
+Bugguie,1
+Stima,1
+Maquina,1
+Travajos de cubierta,1
+Mansory,1
+Bmw 730d,1
+Ventas,1
+Motocarro,1
+Merdedes benz,1
+Dax,1
+Nissa,1
+Yugo,1
+0pel.,1
+Keeway,1
+Gaspardo,1
+coches de boda,1
+Microcar virgo,1
+Informes,1
+CAMPER Mercedes-Benz,1
+Sunbeam,1
+Kiat,1
+Limpieza vehiculos,1
+Hazard,1
+chrysler adaptada,1
+honda FRV,1
+",,,,,dinero rrapido",1
+porta coches,1
+Mercedes adaptado,1
+Polo 1.2 tsi,1
+Documentacion,1
+Peugeot o similar,1
